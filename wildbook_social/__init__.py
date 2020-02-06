660714551393277779f9adadd73ef02c3fb46d9d
mramir71 <51290254+mramir71@users.noreply.github.com>
2020-02-06 12:29:32 -0600

    added iNaturalist

--- wildbook_social/__init__.py
+++ wildbook_social/__init__.py
@@ -1,7 +1,8 @@
 from .Youtube.youtube import YouTube
 from .Twitter.twitter import Twitter
 from .Database.database import Database
+from .iNaturalist.inaturalist import iNaturalist
 
-assert all((YouTube, Twitter, Database))
+assert all((iNaturalist, YouTube, Twitter, Database))
 
 name = "wildbook_social"
