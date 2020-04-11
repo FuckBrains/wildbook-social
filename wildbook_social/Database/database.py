7f4903f7301b0aa3d7faf83455f212285aa32185
Michelle Ramirez <51290254+mramir71@users.noreply.github.com>
2020-04-10 20:11:14 -0500

    cleaned up postsPerWeek method

--- wildbook_social/Database/database.py
+++ wildbook_social/Database/database.py
@@ -30,82 +30,81 @@ class Database:
         res = self.db[collection].find()
         return [x for x in res]
     
+    def _updateItem(self, collection, id, payload):
+        try:
+            self.db[collection].update_one({"_id": id}, {"$set": payload})
+            return True
+        except(e):
+            print("Error updating item", e)
+            return False
+    
     def convertToUTC(self, collection):
-        res = self.db[collection].find({"relevant":None})
+        res = self.db[collection].find({"$or":[{"relevant":None}, {"relevant": True}, {"captive": False}]})
         count = 0
         anticount = 0
         
         for doc in res:
-            date_str = doc['publishedAt']
-#             print(type(date_str))
+            if self.dbName == 'youtube':
+                date_str = doc['publishedAt']
+                field = 'publishedAt'
+            if self.dbName == 'iNaturalist':
+                date_str = doc['time_observed_utc']
+                field = 'time_observed_utc'
+            if self.dbName == 'flickr':
+                date_str = doc['datetaken']
+                field = 'datetaken'
             if type(date_str) == str:
                 doc_id = doc['_id']
                 datetime_obj = dateutil.parser.parse(date_str)
-                #print(doc_id)
-                #print(type(datetime_obj))
-                #print(type(doc['publishedAt']))
-                self.db[collection].update_one({'_id': doc_id}, {'$set':{'publishedAt': datetime_obj}})
+                self.db[collection].update_one({'_id': doc_id}, {'$set':{ field: datetime_obj}})
                 count = count + 1
-                #print(type(doc['publishedAt']))
             else:
-#                 print("datetime object")
                 anticount = anticount + 1
-                continue
-        print("number of documents with UTC strings: ")
-        print(count)
-        print('\n')
-        print("number of documents with datetime objects: ")
-        print(anticount)
-        
+                #continue
+                
     def doStatistics(self, collection, amount):
         i = 1
         while(amount > 0):
             #only retrieve videos to filter that meet the time frame of June 1st, 2019 and forward
             dateStr = '2019-06-01T00:00:00.00Z'
-            timeFrameStart = dateutil.parser.parse(dateStr)
-            item = self.db[collection].find_one({"$and":[{"relevant": None},{"publishedAt":{"$gte": timeFrameStart}}]})
+            self.timeFrameStart = dateutil.parser.parse(dateStr)
+            if self.dbName == 'youtube':
+                item = self.db[collection].find_one({"$and":[{"relevant": None},{"publishedAt":{"$gte": self.timeFrameStart}}]})
+            elif self.dbName == 'twitter':
+                item = self.db[collection].find_one({"$and":[{"relevant": None},{"createdat":{"$gte": self.timeFrameStart}}]})
+            elif self.dbName == 'flickr_june_2019':
+                item = self.db[collection].find_one({'relevant': None})
+            
             if not item:
                 break
-            
             if self.dbName=='youtube':
                 print("{}: {}".format(i, item['title']['original']))
                 display(YouTubeVideo(item['_id']))
             elif self.dbName == 'twitter':
                 display(Image(item['img_url'], height=100, width=200))
-            elif self.dbName == 'iNaturalist':
-                display(Image(item['url'], height=100, width=200))
-    
+            elif self.dbName == 'flickr_june_2019':
+                if item['url_l'] != "":
+                    display(Image(item['url_l'], height=100, width=200))
+                else: 
+                    #to handle images without available url's
+                    rel = False
+
             print("Relevant (y/n):", end =" ")
-            rel = True if input() == "y" else False
-            
+            rel = True if input() == "y" else False 
             #categorize post as wild or captive
+            loc = 0
             if rel == True:
                 print("Wild (y/n):", end =" ")
-                if input() == 'y':
-                    wild = True
-                else:
-                    wild = False
-                    if self.dbName == 'youtube':
-                        loc = 0
+                wild = True if input() == 'y' else False #mark as wild (y) or captive (n)
                 #prompt user with option to enter location only if encounter is wild (YT videos only)
-                if wild == True:
-                    if self.dbName == 'youtube':
-                        if item['recordingDetails']['location'] == None: 
-                            print("Is there a location? (y/n):", end =" ")
-                            if input() == "y": 
-                                print("Enter location (city,country):", end = " ")
-                                loc = input()
-                            else:
-                                loc = 0   
-            #handle irrelevant posts
-            if rel == False:
-                wild = 0 #bc cannot determine a video to be wild if it is not relevant 
-                if self.dbName == 'youtube':
-                    loc = 0 #location does not matter if post is not relevant
-                
+                if self.dbName == 'youtube' and wild == True:
+                    print("Is there a location? (y/n):", end = " ")
+                    if input() == "y": loc = input()
+            if rel == False: wild = 0  #handle irrelevant posts
+
             #update with new values
             if self.dbName == 'youtube':
-                self._updateItem(collection, item['_id'], {"relevant": rel, "wild": wild,"newLocation": loc })
+                self._updateItem(collection, item['_id'], {"relevant": rel, "wild": wild, "newLocation": loc })
                 print("Response saved! Location : {}.\n".format(loc))
             else:
                 self._updateItem(collection, item['_id'], {"relevant": rel, "wild": wild})
@@ -115,15 +114,6 @@ class Database:
             amount -= 1
             i += 1
         print('No more items to proceed.')
-    
-            
-    def _updateItem(self, collection, id, payload):
-        try:
-            self.db[collection].update_one({"_id": id}, {"$set": payload})
-            return True
-        except(e):
-            print("Error updating item", e)
-            return False
         
     def showStatistics(self, collection):
         total = self.db[collection].count_documents({ "$and": [{"relevant":{"$in":[True,False]}}]})
@@ -132,6 +122,7 @@ class Database:
             return
         #relevant count
         relevant_count = self.db[collection].count_documents({ "$and": [{"relevant":True}]})
+        print("relevant: {} \n".format(relevant_count))
         # percent relevant caluclated out of total
         relevant = self.db[collection].count_documents({ "$and": [{"relevant":True}]}) / total * 100 
         # percent wild calculated out of ONLY relevant items, this way the remaining percent can be
@@ -140,14 +131,16 @@ class Database:
         # percent wild calculated out of total
         wild_tot = self.db[collection].count_documents({ "$and": [{"relevant":{"$in":[True]}}, {"wild":True}] }) / total * 100
         print("Out of {} items, {}% are relevant.From those that are relevant, {}% are wild. Out of the total, {}% are wild".format(total, round(relevant,1), round(wild,1), round(wild_tot, 1)))
-
         self.showHistogram(collection)
-
+        
     def showHistogram(self, collection):
-        keys = {'youtube': 'publishedAt', 'twitter': 'created_at', 'iNaturalist': 'time_observed_utc'}
+        #UPDATE: fixed so it only displays delay in time between successive posts in time frame (June 01, 2020)
+        keys = {'youtube': 'publishedAt', 'twitter': 'created_at', 'iNaturalist': 'time_observed_utc', 'flickr': 'datetaken'}
         #gather results for youtube or twitter
-        if self.dbName == 'youtube' or self.dbName == 'twitter':
-            res = self.db[collection].find({'wild':True})
+        if self.dbName == 'youtube' or self.dbName == 'twitter' or self.dbName == 'flickr':
+            dateStr_2 = '2019-06-01T00:00:00.00Z'
+            self.timeFrameStart_2 = dateutil.parser.parse(dateStr_2)
+            res = self.db[collection].find({"$and":[{"wild": True},{"publishedAt":{"$gte": self.timeFrameStart_2}}]})
         else:
             #gather results for iNaturalist
             res = self.db[collection].find({"$and": [{'captive': False},{'time_observed_utc':{"$ne":None}}]})
@@ -156,8 +149,11 @@ class Database:
         if len(self.timePosts) < 1:
             print("No videos were processed yet.")
             return
-        self.dates = [dateutil.parser.parse(x).date() for x in self.timePosts] #Convert the times from datetime format to YYYY-MM-DD format
-        self.dates.sort() #sort the converted dates in a list with most recent at beginning and least recent towards end
+        #IMPORTANT: self.dates() is a list of datetime.date() objects of wild encounters within the time frame
+        #it converts .datetime objs to more general .date objs (easier to work with)
+        #and then sorts the converted dates in a list with most recent at beginning and least recent towards end
+        self.dates = [x.date() for x in self.timePosts] 
+        self.dates.sort() 
         
         # Find the difference in days between posting dates of successive posts
         lastDate = [self.dates[0]]  #stores all the dates we have already looked at
@@ -175,81 +171,56 @@ class Database:
         plt.title('Histogram for Time Between Succesive Wild Posts')
         plt.show()
         
-    #gather all posts within a certain time frame
-    #collection should be species name + wild
-    #fromDate is the date (string) we want to start gathering data at
-    #return a list of dates within time frame
-    def gatherDates(self, collection, YYYY = 2019, MM = 6, DD = 1):
-        self.listOfDates = []
-        for date in self.dates:
-            if date >= datetime.date(year = YYYY, month = MM, day = DD): #fromDate:
-                self.listOfDates.append(date)
-                
-        #print("list of dates from fromDate until now")
-        #print(self.listOfDates)
-        return self.listOfDates
       
-    #take list of dates as input and structure a dictionary as such: {week_0: 2, week_1: 15, week_2: 37 ...}
+    #structures a dictionary as such: {week_0: 2, week_1: 15, week_2: 37 ...} from a list of dates
     #plots number of posts (y axis) vs week # (x axis)
-    def postsPerWeek(self, YYYY = 2019, MM = 6, DD = 1):
-        start = datetime.date(year = YYYY, month = MM, day = DD)
+    def postsPerWeek(self): #, YYYY = 2019, MM = 6, DD = 1):
+        start = self.timeFrameStart_2.date()
         end = datetime.date.today()
-        weekStartDates = []
-        
-        #make a list of dates for the start of each week beginning from "start" to "end"
-        while start < end : 
-            weekStartDates.append(start)
-            start += datetime.timedelta(days = 7)   
-     
-        #make a dictionary to order weekStartDates
-        #format self.dictWeekNumbers = { 1 : 06.01.19, 2: 06.08.19, 3:06.15.19 ... }
-        #where the values are datetime objects 
-        self.dictWeekNumbers = {}
         weekNumber = 1
-        for week in weekStartDates:
-            self.dictWeekNumbers[weekNumber] = week
+        count = 0
+        self.dictWeekNumbers = {}
+        self.postsPerWeekDict = {}
+        numOfPosts = len(self.dates)
+
+        #make a dictionary to order weekStartDates
+        #format of self.dictWeekNumbers = { 1 : 06.01.19, 2: 06.08.19, 3:06.15.19 ... }
+        date = start
+        while date < end:
+            self.dictWeekNumbers[weekNumber] = date 
+            date += datetime.timedelta(days = 7)
             weekNumber += 1
-        print('\n')
-        print("week number dictionary: \n", self.dictWeekNumbers)
-        print('\n')
-            
+        #print("\n week number dictionary: \n", self.dictWeekNumbers)   
+    
         #make a dictionary self.postsPerWeek
         #keys are datetime objects of the date to start a new week
         #values are number of posts that were posted anytime from that date to date + 6 days
-        #format self.postsPerWeek = { 06.01.19 : 4, 06.08.19 : 5, 06.15.19 : 1 ... }
-        count = 0
-        self.postsPerWeekDict = {}
-        for weekStartDate in weekStartDates:
-            nextDate = weekStartDate + datetime.timedelta(days = 7)
-            for date in self.listOfDates:
-                if (date >= weekStartDate) and (date < nextDate):
+        #format self.postsPerWeekDict = { 06.01.19 : 4, 06.08.19 : 5, 06.15.19 : 1 ... }
+        for key,value in self.dictWeekNumbers.items():
+            current_week = value
+            next_week = current_week + datetime.timedelta(days = 7)
+            for date in self.dates:
+                if (date >= current_week) and (date < next_week):
                     count += 1
-            self.postsPerWeekDict[weekStartDate] = count
+            self.postsPerWeekDict[current_week] = count
             count = 0
         
-        return self.postsPerWeekDict
-    
+           
+        return self.postsPerWeekDict, numOfPosts
+            
     #use numpy to compute and plot the smoothed out posts per week stats in order to visualize any trends
     #plot average number of posts (y-axis) vs week # (x axis)
     #returns a list of simple moving average data points
     def movingAveragePosts(self,window):
         #create a list of just the counts of posts for each week 
-        postsPerWeekList = [item for item in self.postsPerWeekDict.values()]
-        print(postsPerWeekList)  
-
+        postsPerWeekList = [item for item in self.postsPerWeekDict.values()] #FIXME: CHECK ORDER DUE TO DICTIONARY 
+        
         #print('calculating simple moving average...\n')
         #calculating moving average with data points in postsPerWeekList
         weights = np.repeat(1.0, window)/window
         self.smas = np.convolve(postsPerWeekList, weights, 'valid') #calculate simple moving averages (smas)
-        return self.smas
+        return self.smas  
     
-    #get videoID's for each document that belongs to a wild encounter
-    #return a list of videoID's
-    def getUserCountriesIDs(self, collection):
-        docs = self.db[collection].find({"wild": True})     
-        listOfVideoIDs = [doc['videoID'] for doc in docs if dateutil.parser.parse(doc['publishedAt']).date() in self.listOfDates] 
-        return listOfVideoIDs
-      
     #customized to youtube only so far
     def heatmap(self,collection, csvName):
         self.csvName = csvName +'.csv'
@@ -275,6 +246,54 @@ class Database:
             for item in loc_list:
                 csvName.writerow(item)
         print('done! Check in your jupyter files for a .csv file with the name you entered')
+        
+    #get videoID's for each document that belongs to a wild encounter within timeframe
+    #self.listOfDates consists of each date that our documents within the timeframe were published at
+    #return a list of videoID's
+    def getVideoIDs(self, collection):
+        docs = self.db[collection].find({"wild": True})     
+        #dateutil.parser.parse(doc['publishedAt']).date()
+        self.listOfVideoIDs = [doc['videoID'] for doc in docs if doc['publishedAt'].date() in self.listOfDates] 
+        return self.listOfVideoIDs
+    
+    #method to compute the number of wild encounter posts a user uploads
+    #configured for the following plastforms so far:
+    #1. YouTube, 2. , 3. ,4.
+    # postsPerUser works by constructing a pandas Dataframe object for each user who posted a wild encounter
+    # columns in the dataframe are: CHANNEL_ID(user), COUNTRY_ABBREVIATION, COUNTRY_FULL, NUM_POSTS
+    # each row of the dataframe would then correspond to a different user
+    #user_countries is a list of dictionaries such that [{ channelID: country_abbreviation}, {...}, {...}]
+    def postsPerUser(self, wild_collection, channel_id_list): #, user_dict_list):
+        #//////for YouTube///////
+        #iterate through the list of channel id's and increase count -- store in user_count_dict 
+        #user_count_dict is structured as {channelId : count, channelId: count, ...}
+        self.list_of_users = [] # structured as [{channelID: count, country: ___ }, ...]
+        user_count_dict = {} 
+        for each_user_id in channel_id_list:
+            if each_user_id not in user_count_dict.keys():
+                user_count_dict[each_user_id] = 0
+            user_count_dict[each_user_id] = user_count_dict[each_user_id] + 1
+#             #go through the user_dict_list and find the country the channel belongs to. Add it to user_count_dict
+#             #fixme: do this efficiently!! FIXME: come back and work on this 
+#             for user_dict in user_dict_list:
+#                 user_count_dict['country'] = user_dict[each_user_id] 
+            #self.list_of_users.append(user_count_dict) #if CSV writing fails delete this
+        
+        #if CSV writing fails, uncomment this - paste what is printed into excel and go from there
+        for user_id in user_count_dict:
+            print(user_id, ':', user_count_dict[user_id])
+        
+        return self.list_of_users
+    
+    def csvWriter(self,list_of_dictionaries, csvName):
+       #fields = ['channelId' ] 
+        with open(csvName, 'w') as new_csv:
+            csvName = csv.DictWriter(new_csv, fieldnames = fields)
+            csvName.writeheader()
+            for item in list_of_dictionaries:
+                csvName.writerow(item)
+        print('done! Check in your jupyter files for a .csv file with the name you entered')
+      
     
     #method to form collections consisting of only wild docs for wildbook api call
     #only tailored towards YouTube currently
@@ -285,7 +304,9 @@ class Database:
         #if not already in newCollection
         for item in newDocs:
             if self.db[newCollection].find_one(item) == None:
-                self.db[newCollection].insert_one(item);
+                self.db[newCollection].insert_one(item)
+            else:
+                pass
                 
     def clearCollection(self, collection, msg=''):
         if (msg == 'yes'):
