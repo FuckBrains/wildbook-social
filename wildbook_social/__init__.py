3f2fb83722d2b64fb846a710eaf7256a6aa078fa
Michelle Ramirez <51290254+mramir71@users.noreply.github.com>
2020-04-12 09:22:32 -0500

    added Flickr

--- wildbook_social/__init__.py
+++ wildbook_social/__init__.py
@@ -1,8 +1,11 @@
 from .Youtube.youtube import YouTube
 from .Twitter.twitter import Twitter
-from .Database.database import Database
 from .iNaturalist.inaturalist import iNaturalist
+from .Flickr.flickr import Flickr
+from .Database.database import Database
+#from .Database_Beta.database_beta import Database_Beta
 
-assert all((iNaturalist, YouTube, Twitter, Database))
+assert all((Flickr, iNaturalist, YouTube, Twitter, Database))
+#assert all((Flickr, iNaturalist, YouTube, Twitter, Database_Beta))
 
 name = "wildbook_social"
